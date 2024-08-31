665acac78acf19e40740b55e93088987d9864dac
prdel24 <brozikmrozik7@gmail.com>
2024-08-31 18:45:03 +0200

    feat: create App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import HeroSection from "./components/HeroSection";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-
-  return (<></>);
+  return (
+    <div className="flex flex-col items-center justify-start font-Satoshi">
+      <Navbar />
+      <HeroSection />
+      <IntroductionSection />
+    </div>
+  );
 }
 
-export default App
+export default App;
