941e16122636ba93b5d01b516401be8d65524a95
prdel24 <brozikmrozik7@gmail.com>
2024-08-31 18:45:28 +0200

    feat: create Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import { useAppThemeStore } from "@/stores/useAppTheme";
+import { motion } from "framer-motion";
+import { useCallback } from "react";
+
+export function Navbar() {
+  const { theme } = useAppThemeStore();
+
+  const handleLogoClick = useCallback(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const handleNavClick = useCallback((section: string) => {}, []);
+
+  const handleBookAppointmentClick = useCallback(() => {}, []);
+
+  const navItems = [
+    { label: "About", onClick: () => handleNavClick("about") },
+    { label: "Services", onClick: () => handleNavClick("services") },
+    { label: "Contact", onClick: () => handleNavClick("contact") },
+  ];
+
+  return (
+    <div className="fixed z-10 flex items-center w-full h-12 px-4 bg-white">
+      <h1
+        className="text-xl font-bold cursor-pointer"
+        style={{ color: theme.headingTextColor }}
+        onClick={handleLogoClick}
+      >
+        PetSln
+      </h1>
+      <div className="w-full h-10"></div>
+      <div className="flex items-center gap-5">
+        {navItems.map((navItem) => (
+          <motion.a
+            className="cursor-pointer"
+            whileHover={{ color: theme.hoverTextColor }}
+            style={{ color: theme.paragraphTextColor }}
+            onClick={navItem.onClick}
+          >
+            {navItem.label}
+          </motion.a>
+        ))}
+        <button className="flex-wrap px-4 py-1 text-white bg-black rounded-full whitespace-nowrap">
+          Book Appointment
+        </button>
+      </div>
+    </div>
+  );
+}
