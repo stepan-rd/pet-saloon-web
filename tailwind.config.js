28e1bd85db17109906b9f078a3f939c536a5d075
prdel24 <brozikmrozik7@gmail.com>
2024-08-31 16:00:36 +0200

    feat: add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "Satoshi": ["Satoshi Variable", "SF pro", "Arial", "Calibri"]
+      }
+    },
   },
   plugins: [],
 }
