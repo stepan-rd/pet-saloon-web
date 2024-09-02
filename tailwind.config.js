d534b16759881cf7b4d9cdf619be8f871224331f
prdel24 <brozikmrozik7@gmail.com>
2024-09-02 19:56:18 +0200

    feat: add gradient background for trustCards

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ export default {
     extend: {
       fontFamily: {
         "Satoshi": ["Satoshi Variable", "SF pro", "Arial", "Calibri"]
-      }
+      },
+      backgroundImage: {
+        'trust-card-bg-gradient': 'linear-gradient(to top, #12FF88, #3DFF9E)',
+      },
     },
   },
   plugins: [],
