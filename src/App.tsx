32121f7fd5a1e0108a125f95ed4b84ea7c7c2f8f
prdel24 <brozikmrozik7@gmail.com>
2024-09-02 19:53:19 +0200

    feat: add more sections to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { CallToActionSection } from "./components/CallToActionSection";
+import { Footer } from "./components/Footer";
 import HeroSection from "./components/HeroSection";
+import { IntroductionSection } from "./components/IntroductionSection";
 import { Navbar } from "./components/Navbar";
+import { PricingSection } from "./components/PricingSection";
+import { ServicesSection } from "./components/ServicesSection";
+import { TestimonialsSection } from "./components/TestimonialsSection";
+import { TrustSection } from "./components/TrustSection";
+
+
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-start font-Satoshi">
+    <div className="flex flex-col items-center justify-start w-full overflow-x-hidden dark-mode-scrollbar scrollbar font-Satoshi">
       <Navbar />
       <HeroSection />
       <IntroductionSection />
+      <ServicesSection />
+      <TrustSection />
+      <TestimonialsSection />
+      <PricingSection />
+      <CallToActionSection />
+      <Footer />
     </div>
   );
 }
