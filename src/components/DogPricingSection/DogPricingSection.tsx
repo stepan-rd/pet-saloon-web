e8ea88f31e6afd2029fb33042d7777c02190ec83
prdel24 <brozikmrozik7@gmail.com>
2024-09-02 19:53:57 +0200

    feat: add DogPricingSection component

---
+++ src/components/DogPricingSection/DogPricingSection.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import animalData from "@/data/animals.json";
+import { Animal } from "@/types/global";
+import { motion } from "framer-motion";
+import { useAppThemeStore } from "@/stores/useAppTheme";
+import { PricingCard } from "./PricingCard";
+import { DogSizeBtns } from "@/components/DogPricingSection/DogSizeBtns";
+
+
+const animal: Animal =
+  animalData.find((animal) => animal.type === "Dog") || ({} as Animal);
+
+type Props = {};
+
+export function DogPricingSection({}: Props) {
+  const { theme } = useAppThemeStore();
+
+  const [currDisplayingSizeIndex, setCurrDisplayingSizeIndex] =
+    useState<number>(2);
+
+  return (
+    <div className="mt-[50px] flex flex-col justify-center items-center px-10">
+      <h1 className="flex justify-center w-full px-10 mb-10 text-3xl font-bold">
+        Dogs
+      </h1>
+      <div className="flex">
+        <DogSizeBtns
+          currDisplayingSizeIndex={currDisplayingSizeIndex}
+          setCurrDisplayingSizeIndex={setCurrDisplayingSizeIndex}
+          animal={animal}
+        />
+        <div className="flex flex-col items-center text-stone-100">
+          <img
+            className="text-black"
+            width={300}
+            height={450}
+            src={animal.sizes[currDisplayingSizeIndex].imgSrc}
+            alt={animal.sizes[currDisplayingSizeIndex].size + " dog"}
+          />
+          <PricingCard currDisplayingSizeIndex={currDisplayingSizeIndex} animal={animal} />
+        </div>
+      </div>
+    </div>
+  );
+}
