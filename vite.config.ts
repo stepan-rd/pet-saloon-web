28eaebe0974ecb0ba9d2c70e12937ff70e48d97d
prdel24 <brozikmrozik7@gmail.com>
2024-09-02 20:12:33 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pet-saloon-web/', // Replace with your GitHub repository name
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src"
+      "@": "/src",
     },
   },
 });
