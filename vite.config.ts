cfa32b31f0a951a7b8ece3aa051eded9fc2b68cd
prdel24 <brozikmrozik7@gmail.com>
2024-09-03 15:41:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pet-saloon-web/', 
   plugins: [react()],
   resolve: {
     alias: {
