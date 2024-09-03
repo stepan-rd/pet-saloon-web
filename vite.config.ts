4ddcce81c22e1145e3bc2e9f39fcf9ac2f2bd83d
prdel24 <brozikmrozik7@gmail.com>
2024-09-03 14:59:18 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pet-saloon-web/', // Update this if your GitHub Pages repo name is different
+  base: '/pet-saloon-web', // Update this if your GitHub Pages repo name is different
   plugins: [react()],
   resolve: {
     alias: {
